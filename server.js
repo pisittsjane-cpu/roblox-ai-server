d970691b5d4ea879b82ece0821a16033826b55c9
pisittsjane-cpu <pisitts.jane@gmail.com>
2026-03-28 12:48:17 +0700

    Update server.js
    import express from "express";
    
    const app = express();
    app.use(express.json());
    
    app.post("/ai", (req, res) => {
        const message = req.body.message;
        res.json({ reply: "AI ตอบ: " + message });
    });
    
    app.get("/", (req, res) => {
        res.send("AI Server Running!");
    });
    
    app.listen(3000, () => {
        console.log("Server started");
    });

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.post("/ai", (req, res) => {
+    const message = req.body.message;
+    res.json({ reply: "AI ตอบ: " + message });
+});
+
+app.get("/", (req, res) => {
+    res.send("AI Server Running!");
+});
+
+app.listen(3000, () => {
+    console.log("Server started");
+});
 
